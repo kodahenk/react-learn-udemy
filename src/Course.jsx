46936b9c123119f5d96e3aeb4771921d8fdb4244
Orhan Osmanoğlu <orhan.osmanoglu@erdemhospital.com>
2023-05-31 13:35:51 +0300

    resimler eklendi

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,4 +1,12 @@
 import Resim1 from './assets/images/resim1.png'
+import Resim2 from './assets/images/resim2.png'
+import Resim3 from './assets/images/resim3.png'
+import Resim4 from './assets/images/resim4.png'
+import Resim5 from './assets/images/resim5.png'
+import Resim6 from './assets/images/resim6.png'
+import Resim7 from './assets/images/resim7.png'
+import Resim8 from './assets/images/resim8.png'
+import Resim9 from './assets/images/resim9.png'
 
 function Course() {
   return (
