58010c8d1a0108c1e7c0b26b9ac2022223c36727
Orhan Osmanoğlu <orhan.osmanoglu@erdemhospital.com>
2023-05-31 21:54:13 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
 import Course from "./Course";
 
-function App() {
-  const [course, setCourse] = useState(11);
+function getRandomCourses() {
+  let imageNumner = Math.floor(Math.random() * 9) + 1;
+  return ["Resim" + imageNumner];
+}
 
+function App() {
+  const [courses, setCourses] = useState([]);
 
   return (
     <>
-      <div className="container mt-3 shadow-sm border p-3">
-        <Course></Course>
+      <div className="container mt-3 p-3">
+        <div className="text-center mb-3">
+          <button
+            onClick={() => {
+              setCourses([...courses, getRandomCourses()]);
+            }}
+            className="btn btn-primary"
+          >
+            Add Course
+          </button>
+        </div>
+        <div className="row">
+          {courses.map((course, index) => {
+            return <Course key={index} image={course}></Course>;
+          })}
+        </div>
       </div>
     </>
   );
--- src/Course.jsx
+++ src/Course.jsx
@@ -1,17 +1,36 @@
-import Resim1 from './assets/images/resim1.png'
-import Resim2 from './assets/images/resim2.png'
-import Resim3 from './assets/images/resim3.png'
-import Resim4 from './assets/images/resim4.png'
-import Resim5 from './assets/images/resim5.png'
-import Resim6 from './assets/images/resim6.png'
-import Resim7 from './assets/images/resim7.png'
-import Resim8 from './assets/images/resim8.png'
-import Resim9 from './assets/images/resim9.png'
+import Resim1 from "./assets/images/resim1.png";
+import Resim2 from "./assets/images/resim2.png";
+import Resim3 from "./assets/images/resim3.png";
+import Resim4 from "./assets/images/resim4.png";
+import Resim5 from "./assets/images/resim5.png";
+import Resim6 from "./assets/images/resim6.png";
+import Resim7 from "./assets/images/resim7.png";
+import Resim8 from "./assets/images/resim8.png";
+import Resim9 from "./assets/images/resim9.png";
 
-function Course() {
+const courseMap = {
+  Resim1,
+  Resim2,
+  Resim3,
+  Resim4,
+  Resim5,
+  Resim6,
+  Resim7,
+  Resim8,
+  Resim9,
+};
+
+function Course({ image }) {
   return (
     <>
-      <div className="alert alert-danger"> alert test</div>
+      <div className="col-md-2 mb-2">
+        <div class="card shadow-sm border border-3 border-dark">
+          <img src={courseMap[image]} className="card-img-top rounded-top-1 " />
+          <div className="card-body border border-top border-dark border-3 border-bottom-0 border-start-0 border-end-0">
+            <p className="card-title text-center">{image}</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
