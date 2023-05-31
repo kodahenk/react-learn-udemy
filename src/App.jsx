f3f09a5caab6fd709a1cbdf365d54fef72c9ecec
Orhan Osmanoğlu <orhan.osmanoglu@erdemhospital.com>
2023-05-31 13:24:47 +0300

    state kullanımı

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // course değişken adı
+  // setfirst değişkene değer atayacağımız fonksiyonA
+  const [course, setfirst] = useState(11);
+
+  const clickBtn = () => {
+    setfirst("call func" + course);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={clickBtn}>clickBtn call</button>
+        <button
+          onClick={() => {
+            setfirst("arrow func inline");
+          }}
+        >
+          arrow func
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>Course count: {course}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
